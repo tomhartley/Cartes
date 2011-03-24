ac93cdac97b5a20f2176aeba1e02189df651f2e8
Tom Hartley <tomhartley@rocketmail.com>
2011-03-24 20:54:41 +0000

    Added files

---
+++ Cartes/Classes/View/THCardView.h
@@ -0,0 +1,16 @@
+//
+//  THCardView.h
+//  Cartes
+//
+//  Created by Tom Hartley on 24/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface THCardView : UIView {
+    
+}
+
+@end
---
+++ Cartes/Classes/View/THCardView.m
@@ -0,0 +1,37 @@
+//
+//  THCardView.m
+//  Cartes
+//
+//  Created by Tom Hartley on 24/03/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "THCardView.h"
+
+
+@implementation THCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
